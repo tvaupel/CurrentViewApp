aca4b49bf70c8d91044f33991f60c588a4e6b4f5
Tom Vaupel <tomvaupel@gmail.com>
2016-11-24 17:35:05 +0000

    Added Dashboard, LoginError and Questionnaire_Menu classes

--- app/src/main/java/uk/ucl/group21/currentviewapp/QuestionnaireMenu.java
+++ app/src/main/java/uk/ucl/group21/currentviewapp/QuestionnaireMenu.java
@@ -3,14 +3,11 @@ package uk.ucl.group21.currentviewapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class LoginScreen extends AppCompatActivity {
+public class QuestionnaireMenu extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_screen);
+        setContentView(R.layout.activity_questionnaire_menu);
     }
-
-
-
 }
