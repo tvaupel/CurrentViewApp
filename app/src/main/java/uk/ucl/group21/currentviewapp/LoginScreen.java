85ab2b61e7989b194204b18138b77baa88e84146
Tom Vaupel <tomvaupel@gmail.com>
2016-11-28 19:38:19 +0000

    Added the login function to the Login Screen

--- app/src/main/java/uk/ucl/group21/currentviewapp/LoginScreen.java
+++ app/src/main/java/uk/ucl/group21/currentviewapp/LoginScreen.java
@@ -1,10 +1,17 @@
 package uk.ucl.group21.currentviewapp;
 
+import android.content.Intent;
+import android.support.annotation.XmlRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginScreen extends AppCompatActivity {
+
+    @XmlRes public EditText acc_number;
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,7 +19,23 @@ public class LoginScreen extends AppCompatActivity {
         setContentView(R.layout.activity_login_screen);
     }
 
-    private void LogIn(View view){
+    public void LogIn(View view){
+
+
+        try {
+
+            if(acc_number.getText().equals("user")) {
+                Intent intent = new Intent(this, Dashboard.class);
+                startActivity(intent);
+            }
+            else {
+                System.out.println("Error: Wrong username or password");
+            }
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+
 
     }
 
