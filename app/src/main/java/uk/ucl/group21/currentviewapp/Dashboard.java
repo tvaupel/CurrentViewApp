24016f9eee372fcdd34465ae14815c2b13d2e640
Tom Vaupel <tomvaupel@gmail.com>
2016-12-01 13:44:21 +0000

    Adding the logout navigation drawer.

--- app/src/main/java/uk/ucl/group21/currentviewapp/Dashboard.java
+++ app/src/main/java/uk/ucl/group21/currentviewapp/Dashboard.java
@@ -47,7 +47,7 @@ public class Dashboard extends AppCompatActivity implements NavigationView.OnNav
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.nav_drawer, menu);
+        getMenuInflater().inflate(R.menu.nav_logout, menu);
         return true;
     }
 
