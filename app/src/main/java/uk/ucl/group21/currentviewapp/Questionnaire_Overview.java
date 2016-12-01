93f487b56947172811f4827c5aa0d1abaf39e5b3
kodariewhs <kodariexd@gmail.com>
2016-12-01 15:31:07 +0000

    Update Questionnaire_Overview.java
    modified by deleting line 19-26

--- app/src/main/java/uk/ucl/group21/currentviewapp/Questionnaire_Overview.java
+++ app/src/main/java/uk/ucl/group21/currentviewapp/Questionnaire_Overview.java
@@ -15,15 +15,5 @@ public class Questionnaire_Overview extends AppCompatActivity {
         setContentView(R.layout.activity_questionnaire__overview);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
-
 }
