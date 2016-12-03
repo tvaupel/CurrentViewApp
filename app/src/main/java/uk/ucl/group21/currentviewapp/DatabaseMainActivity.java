9d9e35d8eea5a334d70a74a5687d097e8fc9a782
Tom Vaupel <tomvaupel@gmail.com>
2016-12-03 23:08:49 +0000

    Changes to layout of database_main.xml

--- app/src/main/java/uk/ucl/group21/currentviewapp/DatabaseMainActivity.java
+++ app/src/main/java/uk/ucl/group21/currentviewapp/DatabaseMainActivity.java
@@ -2,13 +2,10 @@ package uk.ucl.group21.currentviewapp;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -21,8 +18,6 @@ public class DatabaseMainActivity extends AppCompatActivity {
     //create variable for our text and button ;
     EditText editID, editName,editDOB,editDate, editTime,editReasonofCompleting;
     Button button;
-
-
 
 
     @Override
@@ -48,35 +43,31 @@ public class DatabaseMainActivity extends AppCompatActivity {
         button.setOnClickListener(
                 new View.OnClickListener(){
                     @Override
-                    public void onClick(View v){
-
-                        //boolean for : if value is inputed = ture otherwise false)
+                    public void onClick(View v){ //boolean for : if value is inputted = true otherwise false
                         boolean isInserted = myDb.insertData (editID.getText().toString(),
                                 editName.getText().toString(),
                                 editDOB.getText().toString(),
                                 editDate.getText().toString(),
                                 editTime.getText().toString(),
-                                editReasonofCompleting.getText().toString() );
+                                editReasonofCompleting.getText().toString());
 
                         if (isInserted==true) {
-                            Toast.makeText(DatabaseMainActivity.this, "Data Inserted", Toast.LENGTH_LONG).show();
-                            // If data are insert is successful
+                            Toast.makeText(DatabaseMainActivity.this, "Data Inserted", Toast.LENGTH_SHORT).show();
+                            // If data insert is successful
                             Intent real = new Intent(DatabaseMainActivity.this,Q1ToQ10.class); //opens the DatabaseMainActivity
                             startActivity(real);
                         }
                         else {
                             Toast.makeText(DatabaseMainActivity.this, "Data Not Inserted", Toast.LENGTH_LONG).show();
                         }
-
                     }
-                }
-        );
+                });
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main_activities, menu);
+        // No addition
         return true;
     }
 
