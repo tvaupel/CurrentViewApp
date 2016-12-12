22921aec697e7060e611349f7cd2db6792e7c470
jaimin200 <jaimin200@hotmail.com>
2016-12-12 11:41:39 +0000

    Same as jai_working_2 but with user login and database incorported

---
+++ app/src/main/java/com/aditya/adityapc/sqliteloginregistrationexample/LoginError.java
@@ -0,0 +1,13 @@
+package com.aditya.adityapc.sqliteloginregistrationexample;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class LoginError extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_error);
+    }
+}
---
+++ app/src/main/java/com/aditya/adityapc/sqliteloginregistrationexample/Setting.java
@@ -0,0 +1,13 @@
+package com.aditya.adityapc.sqliteloginregistrationexample;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Setting extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+    }
+}
